1a2cf17894dcf13a4ccdd49bffce8e5a8e44be94
Santiago Torres <santiago_oziel92@hotmail.com>
2026-03-09 13:16:03 -0600

    wip

--- api/src/managers/deck-manager/deck.manager.ts
+++ api/src/managers/deck-manager/deck.manager.ts
@@ -2,9 +2,10 @@ import { knownError } from "src/lib/error-models";
 import { AttemptToFetch, failure } from "src/lib/functors";
 import { ArchetypeName } from "src/repositories/archetype-repo/archetype.models";
 import { DeckID } from "src/repositories/deck-repo/deck.models";
+import { DeckRepository } from "src/repositories/deck-repo/deck.repo";
 
 export class DeckManager {
-    constructor() {}
+    constructor(private readonly deckRepository: DeckRepository) {}
 
     getArchetypes = async (deckId: DeckID): AttemptToFetch<{ archetypes: ArchetypeName[] }> => {
         return failure(knownError('Not implemented yet'))
